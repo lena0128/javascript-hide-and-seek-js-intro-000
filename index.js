d892ca3bb039edea1cd94c1bbb1357859c162d07
Lena Hou <lenahou2013@gmail.com>
2020-12-20 15:56:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ return document.getElementById("nested").querySelector("div.target");
 }
 
 function increaseRankBy(n){
- const myRank = document.querySelectorAll('ranked-list');
+ const myRank = document.getElementById('app').querySelectorAll('ul.ranked-list');
  for (let i = 0; i < myRank.length; i++) {
    myRank[i].innerHTML = ((parseInt(myRank[i].innerHTML)) + n);
  }
